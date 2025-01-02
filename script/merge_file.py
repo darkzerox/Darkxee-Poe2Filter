14fca8fb94e45d5a313dd8acbeca523e29e914b8
Light <chittaks@scg.com>
2025-01-02 18:57:24 +0700

    test: fix window path

--- script/merge_file.py
+++ script/merge_file.py
@@ -5,10 +5,10 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 # Get project root (parent directory of script)
 project_path = os.path.dirname(script_dir)
 # print(f'Project root: {project_path}')
-filterGroupPath = project_path + "/filter_group/"
+filterGroupPath = os.path.join(project_path, "filter_group")
 
 def merge_files_from_array(file_paths, output_file_name):
-    output_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../', output_file_name + '.filter')
+    output_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', output_file_name + '.filter')
     
     try:
         with open(output_file, 'w') as outfile:
@@ -28,5 +28,8 @@ def merge_files_from_array(file_paths, output_file_name):
 
 if __name__ == "__main__":
     # Example usage
-    test_files = [filterGroupPath+"show/rarity_magic.filter", filterGroupPath+"show/rarity_rare.filter"]
+    test_files = [
+        os.path.join("show", "rarity_magic.filter"),
+        os.path.join("show", "rarity_rare.filter")
+    ]
     merge_files_from_array(test_files, "test")
